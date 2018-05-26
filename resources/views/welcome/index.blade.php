00bb845e8a629485e3f8ef9a627f7ae59224965b
John <eveanyee@gmail.com>
2018-05-26 01:18:58 +0100

    John

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -5,9 +5,11 @@
 
 
 @section('content')
-
-<div class="col-md-12">
-<p>This is my body content.</p>
-</div>
+<div class="container">
+	
+	<div class="col-md-12">
+		<p>This is my body content.</p>
+	</div>
     
+</div>
 @endsection
