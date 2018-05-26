dd799d814eca6121a16d800a9c6aab5898cdcb67
John <eveanyee@gmail.com>
2018-05-26 06:29:37 +0100

    John

--- resources/views/admin/all_users.blade.php
+++ resources/views/admin/all_users.blade.php
@@ -4,6 +4,69 @@
 
 
 
+
+<style>
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+.switch input {display:none;}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+</style>
+
+
+
+
+
 @section('content')
 <div class="container">
     <div class="panel panel-default">
@@ -22,6 +85,7 @@
                                         <th>Email</th>
                                         <th>Reg Date</th>
                                         <th>Status</th>
+                                        <th>Action</th>
                                     </tr>
                                 </thead>
                                   <?php $rows = 0; ?> 
@@ -32,14 +96,31 @@
                         <td>{{ $user->name }} </td>
                            <td> {{ $user->username }}</td>
                         <td>{{ $user->email }}</td>
-                        <td>{{ $user->created_at }}</td>
+                        <td>{{ $user->created_at }} </td>
                         <td> 
                             @if($user->isOnline())
                                 <li class="text-success">Online</li>
                             @else
                                 <li  class="text-danger">Offline</li>
                             @endif
-                        </td>
+	
+</td>
+
+
+<td>
+
+<form>
+<label class="radio-inline">
+  <input type="radio" name="inlineRadioOptions" id="status" value="1"> On
+</label>
+<label class="radio-inline">
+  <input type="radio" name="inlineRadioOptions" id="status" value="0" checked> Off
+</label>
+<button type="button" class="btn btn-primary btn-xs">Submit</button>
+  </form>  
+</td>
+
+
                     </tr>
                     
                 </tbody>
@@ -55,6 +136,36 @@
             </div>
     </div>
 </div>
+
+
+
+<script>
+	$('.btn-toggle').click(function() {
+    $(this).find('.btn').toggleClass('active');  
+    
+    if ($(this).find('.btn-primary').size()>0) {
+    	$(this).find('.btn').toggleClass('btn-primary');
+    }
+    if ($(this).find('.btn-danger').size()>0) {
+    	$(this).find('.btn').toggleClass('btn-danger');
+    }
+    if ($(this).find('.btn-success').size()>0) {
+    	$(this).find('.btn').toggleClass('btn-success');
+    }
+    if ($(this).find('.btn-info').size()>0) {
+    	$(this).find('.btn').toggleClass('btn-info');
+    }
+    
+    $(this).find('.btn').toggleClass('btn-default');
+       
+});
+
+$('form').submit(function(){
+	alert($(this["options"]).val());
+    return false;
+});
+</script>
+
 @endsection
 
 
