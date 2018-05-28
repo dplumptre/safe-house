510ed1d976f1ba9ffca4455728a0bb4d0a5b8698
John <eveanyee@gmail.com>
2018-05-28 14:58:55 +0100

    John

--- app/helpers.php
+++ app/helpers.php
@@ -1,12 +1,5 @@
 <?php
 
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-Use Auth;
-Use App\User;
-Use App\Transaction;
-
 
 function account_number($length) {
     $characters = '0123456789';
@@ -18,14 +11,6 @@ function account_number($length) {
     return $randomString;
 }
 
-
-// function account_balance($id) {
-// 	$transactions = Transaction::where('user_id', $id)->orderBy('created_at', 'desc')->get();
-// 	$total_credit = $transactions->sum('credit');
-// 	$total_debit  = $transactions->sum('debit');
-// 	$account_balance = $total_credit - $total_debit;
-//     return $account_balance;
-// }
 
 
 
