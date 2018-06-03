0468d7e6628ae2f122385192c1e314d4d96558b7
John <eveanyee@gmail.com>
2018-06-03 19:22:10 +0100

    john

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,28 +5,7 @@
 
 <head>
 
-<!-- Basic -->
-<meta charset="utf-8">
-<title>{{ config('app.name', 'Acorn Bank ::. Home') }}</title>
-<meta name="description" content="Ascorn Bank Nigeria , Canada">
-<meta name="author" content="Ascorn Bank - Nogeria , Canada">
-<link rel="shortcut icon" href="boa-favicon.png')}}" type="image/png" />
 
-<!-- Mobile Metas -->
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<!-- Facebook Metas -->
-<meta property="og:title" content="As strong as a group, as close as a partner" />
-<meta property="og:site_name" content="Bank of Africa Uganda"/>
-<meta property="og:url" content="index-2.html" />
-<meta property="og:description" content="Ascorn Bank Nigeria , Canada." />
-<meta property="fb:app_id" content="419634918245424" />
-<meta property="og:type" content="website" />
-<meta property="og:locale" content="en_US" />
-<meta property="og:image" content="facebook/boa-uganda-generic.jpg')}}" />
-<meta property="og:image:type" content="image/jpeg" />
-<meta property="og:image:width" content="1200" />
-<meta property="og:image:height" content="600" />
 
 <!-- Web Fonts  -->
 <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800%7CShadows+Into+Light" rel="stylesheet" type="text/css">
@@ -72,7 +51,7 @@
   <header id="header" class="colored flat-menu darken-top-border">
     <div class="header-top">
       <div class="container">
-        <p>Call Center <span><i class="fa fa-phone"></i><a href="#">+256 414 302 111</a></span> | Toll Free Number <span><i class="fa fa-phone"></i><a href="#">0800 100 140</a></span> | <a href="#">feedback@acornbank.com</a> | <a href="#">Contact Us</a></p>
+        <p>Call Center <span><i class="fa fa-phone"></i><a href="#">+256 414 302 111</a></span> | Toll Free Number <span><i class="fa fa-phone"></i><a href="#">0800 100 140</a></span> | <a href="#">feedback@acornbank.com</a> | <a href="{{ route('login') }}">login</a></p>
         
 
 <div class="search">
@@ -109,147 +88,11 @@
        
           <ul class="nav nav-pills nav-main" id="mainMenu">
           @guest
-            <li class="dropdown mega-menu-item mega-menu-fullwidth active"> <a class="dropdown-toggle" href="#"> Personal <i class="fa fa-angle-down"></i> </a>
-              <ul class="dropdown-menu">
-                <li>
-                  <div class="mega-menu-content">
-                    <div class="row">
-                      <div class="col-md-3">
-                        <ul class="sub-menu">
-                          <li> <span class="mega-menu-sub-title">CURRENT ACCOUNTS</span>
-                            <ul class="sub-menu">
-                              <li><a href="#">Personal Current Account</a></li>
-                            </ul>
-                          </li>
-                        </ul>
-                      </div>
-                     
-                      <div class="clearfix clear"></div>
-                      <div class="col-md-3">
-                        <ul class="sub-menu">
-                          <li> <span class="mega-menu-sub-title">Acorn Packs</span>
-                            <ul class="sub-menu">
-                              <li><a href="#">Ambitions Savings Plan</a></li>
-                              <li><a href="#">Salary Pack</a></li>
-                              <li><a href="#">MY KIKAPU - My Business Pack</a></li>
-                              <li><a href="#">Public Service Pack</a></li>
-                            </ul>
-                          </li>
-                        </ul>
-                      </div>
-                      <div class="col-md-3">
-                        <ul class="sub-menu">
-                          <li> <span class="mega-menu-sub-title">PRIVATE BANKING</span>
-                            <ul class="sub-menu">
-                              <li><a href="#">Private Personal Banking</a></li>
-                            </ul>
-                          </li>
-                        </ul>
-                      </div>
-                      <div class="col-md-3">
-                        <ul class="sub-menu">
-                          <li> <span class="mega-menu-sub-title">FOR YOU</span>
-                            <ul class="sub-menu">
-                              <li><a href="#">Acorn Downloads</a></li>
-                              <li><a href="#" target="_blank">Account Opening Form</a></li>
-                              <li><a href="#">Instant Loan Calculator</a></li>
-                            </ul>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-            </li>
-            <li class="dropdown mega-menu-item mega-menu-fullwidth"> <a class="dropdown-toggle" href="#"> Business <i class="fa fa-angle-down"></i> </a>
-         
-              <ul class="dropdown-menu">
-                <li>
-                  <div class="mega-menu-content">
-                    <div class="row">
-                      <div class="col-md-3">
-                        <ul class="sub-menu">
-                          <li> <span class="mega-menu-sub-title">CORPORATE DEPOSITS</span>
-                            <ul class="sub-menu">
-                              <li><a href="#">Terms Deposits</a></li>
-                              <li><a href="#">Single Fee Business Account</a></li>
-                              <li><a href="#">Remunerated Current Account</a></li>
-                              <li><a href="#">BOA Business Current Account</a></li>
-                              <li><a href="#">School Fees Collection Account</a></li>
-                              <li><a href="#">Embassy/ NGO Current Account</a></li>
-                              <li><a href="#">Embassy Staff Current Account</a></li>
-                            </ul>
-                          </li>
-                        </ul>
-                      </div>
-                      <div class="col-md-3">
-                        <ul class="sub-menu">
-                          <li> <span class="mega-menu-sub-title">CORPORATE LOANS</span>
-                            <ul class="sub-menu">
-                              <li><a href="#">BOA Tax Loan</a></li>
-                              <li><a href="#">Term Loans</a></li>
-                              <li><a href="#">Working Capital</a></li>
-                              <li><a href="#">Overdrafts</a></li>
-                              <li><a href="#">Bonds &amp; Guarantees</a></li>
-                              <li><a href="#">Invoice Discounting</a></li>
-                              <li><a href="#">Contract Financing</a></li>
-                              <li><a href="#">Trade Finance</a></li>
-                              <li><a href="#">Insurance Premium Financing</a></li>
-                            </ul>
-                          </li>
-                        </ul>
-                      </div>
-                      <div class="col-md-3">
-                        <ul class="sub-menu">
-                          <li> <span class="mega-menu-sub-title">SME DEPOSITS</span>
-                            <ul class="sub-menu">
-                              <li><a href="#">School Fees Collection Account</a></li>
-                              <li><a href="#">Ascorn Business Current Account</a></li>
-                              <li><a href="#">Terms Deposits</a></li>
-                              <li><a href="#">Single Fee Business Account</a></li>
-                              <li><a href="#">Remunerated Current Account</a></li>
-                            </ul>
-                          </li>
-                        </ul>
-                      </div>
-                      <div class="col-md-3">
-                        <ul class="sub-menu">
-                          <li> <span class="mega-menu-sub-title">SME FINANCING</span>
-                            <ul class="sub-menu">
-                              <li><a href="#">Unsecured Bid Guarantee</a></li>
-                              <li><a href="#">Business Finance Loan</a></li>
-                              <li><a href="#">Asset Finance Loan</a></li>
-                              <li><a href="#">School Bridge Finance loan</a></li>
-                              <li><a href="#">Supplier Finance</a></li>
-                              <li><a href="#">Trade Finance</a></li>
-                              <li><a href="#">Overdrafts</a></li>
-                              <li><a href="#">Commercial Loans</a></li>
-                              <li><a href="#">Bonds &amp; Guarantees</a></li>
-                              <li><a href="#">Insurance Premium Financing</a></li>
-                          
-                            </ul>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-            </li>
-            <li><a href="#">Mobile Wallet</a></li>
-            <li class="dropdown"> <a class="dropdown-toggle" href="#"> Other Services <i class="fa fa-angle-down"></i> </a>
-              <ul class="dropdown-menu">
-              @if (Auth::user() && Auth::user()->status == "1" && Auth::user()->role_slug == "admin")      
-              <li><a href="{{ route('all_users')}}">View all users</a></li>
-              <li><a href="{{ route('transactions')}}">Perform Transaction</a></li>
-              <li><a href="{{ route('all_users')}}">View all users</a></li>
-              <li><a href="{{ route('transactions')}}">Perform Transaction</a></li>
-              <li><a href="{{ route('transfer')}}">Transfer Fund</a></li>
-             @endif
-              </ul>
-            </li>
-           
+            
+            <li><a href="{{ asset('/') }}">Home</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="{{ route('login') }}">Mobile Wallet</a></li>
+
             <li><a href="{{ route('login')}}" ><i class="fa fa-lock"></i> <span class="long-word">Online Banking Login</span><span class="short-word">Login</span></a></li>
             <li><a href="{{ route('register')}}">Register</a></li>
             @endguest
@@ -272,7 +115,7 @@
 <footer id="footer">
   <div class="container">
     <div class="row">
-      <div class="col-sm-2">
+      <div class="col-sm-3">
         <div class="feature-box">
           <h4 class="shorter">Personal</h4>
           <ul class="contact">
@@ -285,7 +128,7 @@
           </ul>
         </div>
       </div>
-      <div class="col-sm-2">
+      <div class="col-sm-3">
         <div class="feature-box">
           <h4 class="shorter">Business</h4>
           <ul class="contact">
@@ -296,7 +139,7 @@
           </ul>
         </div>
       </div>
-      <div class="col-sm-2">
+      <div class="col-sm-3">
         <div class="feature-box">
           <h4 class="shorter">Other Services</h4>
           <ul class="contact">
@@ -307,20 +150,7 @@
           </ul>
         </div>
       </div>
-      <div class="col-sm-3">
-        <div class="contact-details">
-          <h4 class="shorter">Contacts</h4>
-          <ul class="contact">
-            <li>Plot 45, Jinja Road</li>
-            <li>P.O. Box 2750, Kampala</li>
-            <li>Call Center: <a href="#">+256 414 302 111</a></li>
-            <li>Toll Free Line: <a href="#">0800 100 140</a></li>
-            <li>Whatsapp: <a href="#"> +256 717 800 508</a></li>
-            <li>&nbsp;</li>
-            <li><a href="#"><i class="fa fa-arrow-circle-o-right"></i>All Branches &amp; ATMs</a></li>
-          </ul>
-        </div>
-      </div>
+      
       <div class="col-sm-3">
         <div class="feature-box">
           <h4 class="shorter">Instant Loan Calculator</h4>
@@ -357,7 +187,7 @@
       <div class="col-sm-5">
         <div class="newsletter">
           <h4>Newsletter Subscription</h4>
-          <p>Keep updated with all developments from Bank of Africa Uganda.</p>
+          <p>Keep updated with all developments Acorn Bank.</p>
           <div class="alert alert-success hidden" id="newsletterSuccess"> <strong>Success!</strong> You've been added to our email list. </div>
           <div class="alert alert-danger hidden" id="newsletterError"></div>
           <form id="newsletterForm" action="#" method="POST">
@@ -380,14 +210,7 @@
             <li class="googleplus"><a href="#" target="_blank" data-placement="bottom" rel="tooltip" title="Google Plus">Google Plus</a></li>
           </ul>
         </div>
-        <div class="clear clearfix"></div>
-        <h4>Whatsapp</h4>
-        <div class="social-icons">
-          <ul class="social-icons">
-            <li class="stumbleupon"><a data-placement="bottom" rel="tooltip" title="Whatsapp:  +256 717 800 508">Whatsapp:  +256 717 800 508</a></li>
-            <li class="description"> +256 717 800 508</li>
-          </ul>
-        </div>
+       
       </div>
     </div>
   </div>
@@ -395,8 +218,7 @@
     <div class="container">
       <div class="row">
         <div class="col-md-8">
-          <p>2016 &copy; Bank of Africa Uganda Ltd. All Rights Reserved.<br />
-            BANK OF AFRICA IN UGANDA (Licence Number A1.001) is regulated by BANK OF UGANDA</p>
+          <p>2018 &copy; Acorn Bank Ltd. All Rights Reserved.<br />
         </div>
       </div>
     </div>
@@ -485,5 +307,5 @@
 
 </body>
 
-<!-- Mirrored from www.boauganda.com/index.php by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 08 Apr 2018 12:26:23 GMT -->
+
 </html>
